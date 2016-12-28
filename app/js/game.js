091e3d07919f2bdf469c8a6925b5e4ca7d885929
drewgonzales@alfred <drewgonzales360@gmail.com>
2016-12-28 15:04:02 -0800

    Finished with no bugs
    Currently, there are no bugs in the system and I've optimized
    the drawing functions of the game.
    
    Changes to gameplay:
    	There is no longer a floor where you can always land. If
    	you miss a landing, you will die and the game will restart.
    
    Next on the list:
    	Add flying enemies and menu changes, comments and such.

--- app/js/game.js
+++ app/js/game.js
@@ -13,56 +13,143 @@ const myEmitter = new MyEmitter();
 
 const canvas      = document.getElementById("map");
 const ctx         = canvas.getContext("2d");
-const key         = require('key-emit')(document);
-const ipc         = require('electron').ipcRenderer;
-
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+const buffer      = document.createElement("canvas");
+const bufctx      = buffer.getContext("2d");
+bufctx.canvas.width = window.innerWidth;
+bufctx.canvas.height = window.innerHeight;
+
+const key         = require('key-emit')(document);
+
+
 const TILE_SIZE   = 50;
-const VIEW_WIDTH  = 16;
-const VIEW_HEIGHT = 12;
 const LANDING     = 15;
 const SPEED       = 7;
 const FLOOR       = ctx.canvas.height - TILE_SIZE; // feet of character on ground
-const TIME_RATE   = 0.03;
+const TIME_RATE   = 0.02;
 const FRAME_RATE  = 20;
-var landingIndex  = -1 // code for ground.
 var landings      = [];
-var canJump       = true;
-// as if in the game.
-function drawFloor() {
-  ctx.fillStyle = "black"
-  ctx.fillRect(0, ctx.canvas.height - TILE_SIZE, ctx.canvas.width, TILE_SIZE)
-}
+
+var video = document.createElement('video');
+video.src = '../assets/sadmachine.mp4';
+video.play();
+video.muted = true;
+
 
 var Molly = {
-  height: 0,
+  height: ctx.canvas.height - TILE_SIZE,
   maxHeight: 0,
+  landing: -2,
   draw: function(){
-    if(this.height <  0){
-      this.height = 0;
-    }
-    ctx.fillStyle = "green";
-    ctx.fillRect( TILE_SIZE, FLOOR-TILE_SIZE-this.height, TILE_SIZE, TILE_SIZE)
+    bufctx.fillStyle = "green";
+    bufctx.fillRect( TILE_SIZE, FLOOR-TILE_SIZE-this.height, TILE_SIZE, TILE_SIZE)
   }
 }
 function gravity(t, initHeight){
-  return -600*Math.pow(t,2) + initHeight;
+  return Math.floor(-600*Math.pow(t,2) + initHeight);
 }
 
 function jump(t, iH) {
-  return -600*Math.pow(t,2) + 800*t + iH;
+  return Math.floor(-600*Math.pow(t,2) + 800*t + iH);
 }
+
+
+
+function Landing() {
+  this.x = ctx.canvas.width;      // Current x position on viewport
+  this.y = ctx.canvas.height/2;   // Current y position on viewport
+  this.l = 400;                   // Length of the landing
+  this.s = 3 * Math.random();     // Speed of the landing.
+  this.landable = false;          // Is the landing beneath me?
+  this.floor = false
+}
+
+Landing.prototype.init = function () {
+  // Restart
+  if (this.l + this.x < 0) {
+    this.x = ctx.canvas.width;
+    this.y = (ctx.canvas.height - TILE_SIZE) * Math.random() ;
+    this.l = Math.floor(100 * Math.random()) + 400;
+    this.s = Math.floor(2 * Math.random()) + 5;
+  } else {
+    this.x -= this.s;
+  }
+  if (this.x <= 2*TILE_SIZE && TILE_SIZE <= this.x + this.l && this.y <= Molly.height) {
+    this.landable = true;
+  } else {
+    this.landable = false;
+  }
+
+  // Floor restart
+  drawRect(this.x, this.y, this.l);
+};
+
+landings = [];
+for (var i = 0; i < 8; i++) {
+  landings.push(new Landing());
+}
+
+function start() {
+  myEmitter.emit('fall')
+  var animation=window.setInterval(function() {
+    bufctx.drawImage(video,0, 0, ctx.canvas.width, ctx.canvas.height);
+    Molly.draw();
+
+    for (var i = 0; i < landings.length; i++) {
+      landings[i].init();
+    }
+
+    ctx.drawImage(buffer, 0, 0);
+    if (Molly.landing > -1 && !landings[Molly.landing].landable && closeEnough(Molly.height, landings[Molly.landing].y)) {
+      Molly.landing = -1
+      console.log("Falling off ledge");
+      myEmitter.emit('fall');
+    }
+    if (video.ended || Molly.height <= 0) {
+      clearInterval(animation);
+      window.location.href = `file://${__dirname}/../html/menu.html`    }
+  },FRAME_RATE);
+}
+start();
+
+function drawRect(x, height, length) {
+  bufctx.fillStyle = "black";
+  bufctx.fillRect(x, FLOOR-height, length, LANDING);
+}
+
+function closeEnough(molsHeight, y) {
+  if (Math.floor(Math.abs(molsHeight-y)) < 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+myEmitter.on('fall', () => {
+  let t = 0;
+  let fallin = setInterval(function () {
+    Molly.height = gravity(t, Molly.maxHeight);
+    t += TIME_RATE;
+    for (var i = 0; i < landings.length; i++) {
+      if (landings[i].landable && closeEnough(Molly.height, landings[i].y)) {
+        Molly.height = landings[i].y;
+        Molly.maxHeight = Molly.height;
+        Molly.landing = i;
+        clearInterval(fallin)
+      }
+    }
+  }, FRAME_RATE);
+});
 
 // this function will only handle going up.
 key.pressed.on("space", function(){
-  if (!canJump) {
-    console.log("Cant jump.");
+  if (Molly.landing == -1) {
     return;
   }
-  canJump = false;
   let iHeight = Molly.height;
+  Molly.landing = -1
   let t = 0;
   let air = setInterval(function () {
     Molly.height = jump(t, iHeight);
@@ -76,99 +163,11 @@ key.pressed.on("space", function(){
   }, FRAME_RATE);
 });
 
-var video = document.createElement('video');
-video.src = '../assets/sadmachine.mp4';
-video.play();
-video.muted = true;
-
-class Landing {
-  constructor() {
-    this.x = ctx.canvas.width;      // Current x position on viewport
-    this.y = ctx.canvas.height/2;   // Current y position on viewport
-    this.l = 400;                   // Length of the landing
-    this.s = 3 * Math.random();     // Speed of the landing.
-    this.landable = false;          // Is the landing beneath me?
+key.pressed.on("f", function(){
+  if (Molly.landing == -1) {
+    return;
   }
-  init() {
-    // Restart
-    if (this.x + this.l < 0) {
-      this.x = ctx.canvas.width;
-      this.y = (ctx.canvas.height - TILE_SIZE) * Math.random() ;
-      this.l = 100 * Math.random() + 200;
-      this.s = 2 * Math.random() + 5;
-    } else {
-      this.x -= this.s;
-    }
-    if (this.x <= 2*TILE_SIZE && TILE_SIZE <= this.x + this.l && this.y <= Molly.height) {
-      this.landable = true;
-    } else {
-      this.landable = false;
-    }
-    drawRect(this.x, this.y, this.l);
-  }
-}
+  Molly.height -= 6
+  myEmitter.emit('fall')
 
-var l1 = new Landing();
-var l2 = new Landing();
-var l3 = new Landing();
-var l4 = new Landing();
-landings = [l1, l2, l3, l4];
-
-function start() {
-  var animation=window.setInterval(function() {
-    ctx.drawImage(video,0, 0, ctx.canvas.width, ctx.canvas.height-TILE_SIZE);
-    Molly.draw();
-    drawFloor();
-    l1.init();
-    l2.init();
-    l3.init();
-    l4.init();
-    if (landingIndex > -1 && !landings[landingIndex].landable && closeEnough(Molly.height, landings[landingIndex].y)) {
-      landingIndex = -2; // code for falling
-      console.log("Falling off ledge");
-      myEmitter.emit('fall');
-    }
-    if (video.ended) {
-      clearInterval(animation);
-      window.location.href = `file://${__dirname}/../html/menu.html`    }
-  },FRAME_RATE);
-}
-start();
-
-function drawRect(x, height, length) {
-  ctx.fillStyle = "black";
-  ctx.fillRect(x, FLOOR-height, length, LANDING);
-}
-
-function closeEnough(molsHeight, y) {
-  if (Math.abs(molsHeight-y) < 30) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-myEmitter.on('fall', () => {
-  let t = 0;
-  canJump = false;
-  let fallin = setInterval(function () {
-    Molly.height = gravity(t, Molly.maxHeight);
-    t += TIME_RATE;
-    if ( closeEnough(Molly.height, 0)) {
-      canJump = true;
-      Molly.height = 0;
-      landingIndex = -1;
-      Molly.maxHeight = 0;
-      clearInterval(fallin)
-    }
-    for (var i = 0; i < landings.length; i++) {
-      if (landings[i].landable && closeEnough(Molly.height, landings[i].y)) {
-        canJump = true;
-        Molly.height = landings[i].y;
-        Molly.maxHeight = Molly.height;
-        landingIndex = i;
-        clearInterval(fallin)
-      }
-    }
-  }, FRAME_RATE);
 });
--- app/js/level1.js
+++ app/js/level1.js
@@ -1,4 +1,4 @@
 var Game = require('./game')
 
 var track = new Audio("../assets/colors.mp3")
-track.play();
+// track.play();
